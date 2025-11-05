76e34bad1363afca446610703f436a320d18e9bd
damianmore <dmore0126@gmail.com>
2025-11-05 14:38:54 -0800

    Added health endpoint for monitoring backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("events/", include("app.urls")),
+    path("health/", include("app.health_url"))
 ]
 
