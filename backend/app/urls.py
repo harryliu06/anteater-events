d037d431ccc79d414c28b2a6fa790887a251ce28
damianmore <dmore0126@gmail.com>
2025-11-05 23:11:09 -0800

    Added search endpoint url

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventDetailView, EventListView
+from .views import EventDetailView, EventListView, EventSearchView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),                 # GET /api/events
     path("<uuid:event_id>/", EventDetailView.as_view(), name="event_detail"),  # GET /api/events/<uuid>/
+    path("search/", EventSearchView.as_view(), name="event_search") # GET events/search
 ]
