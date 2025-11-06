7f6f9eabd9e6a7f19197fe3e7200f821cefe4b95
damianmore <dmore0126@gmail.com>
2025-11-06 00:26:47 -0800

    Set up gemini API url endpoint

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EventDetailView, EventListView, EventSearchView
+from .views import EventDetailView, EventListView, EventSearchView,EventAIView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),                 # GET /api/events
     path("<uuid:event_id>/", EventDetailView.as_view(), name="event_detail"),  # GET /api/events/<uuid>/
-    path("search/", EventSearchView.as_view(), name="event_search") # GET events/search
+    path("search/", EventSearchView.as_view(), name="event_search"), # GET events/search
+    path("aisearch/", EventAIView.as_view(), name="event_search") # GET events/search
 ]
