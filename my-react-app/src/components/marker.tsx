f945a6cf24ccd4dbf6f546d8b36712cb8a769513
Harry <thureinlwin2023@gmail.com>
2025-11-07 00:32:30 -0800

    Another adjustment for deployment

--- my-react-app/src/components/marker.tsx
+++ my-react-app/src/components/marker.tsx
@@ -1,5 +1,4 @@
 import mapboxgl from 'mapbox-gl'
-import React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import RoomTwoToneIcon from '@mui/icons-material/RoomTwoTone';
 
