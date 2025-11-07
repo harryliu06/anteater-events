209cd32dba90ab30f64851ab1cb92517d4b02e27
Harry <thureinlwin2023@gmail.com>
2025-11-07 03:14:31 -0800

    Hide imports for deployment

--- my-react-app/src/components/FloatingActionButton.tsx
+++ my-react-app/src/components/FloatingActionButton.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import NavigationIcon from '@mui/icons-material/Navigation';
+// import NavigationIcon from '@mui/icons-material/Navigation';
 
 type Props = {
   onCreate?: () => void
