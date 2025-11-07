ef3f527002f31a805d16ac6bfe8acf22cf909505
Harry <thureinlwin2023@gmail.com>
2025-11-06 21:10:47 -0800

    Delete the edit button

--- my-react-app/src/components/FloatingActionButton.tsx
+++ my-react-app/src/components/FloatingActionButton.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
 import NavigationIcon from '@mui/icons-material/Navigation';
 
 type Props = {
@@ -11,15 +10,12 @@ type Props = {
   onNavigate?: () => void
 }
 
-export default function FloatingActionButtons({ onCreate, onEdit, onNavigate }: Props) {
+export default function FloatingActionButtons({ onCreate, onNavigate }: Props) {
   return (
     <div className="fab-container">
       <Box sx={{ '& > :not(style)': { m: 1 } }}>
         <Fab color="primary" aria-label="add" onClick={onCreate}>
           <AddIcon />
-        </Fab>
-        <Fab color="secondary" aria-label="edit" onClick={onEdit}>
-          <EditIcon />
         </Fab>
         <Fab variant="extended" onClick={onNavigate}>
           <NavigationIcon sx={{ mr: 1 }} />
